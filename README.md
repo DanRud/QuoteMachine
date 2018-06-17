543854474ebb28cabe9191e8b5200dd4e8152590
Daniel <pgup4294@gmail.com>
2018-06-17 16:34:27 +0200

    add quote machine

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quote-Machine (jQuery)
+
+Every time the quote is drawn, the background color also change(array colors) 
+https://forismatic.com/en/api/
+
+
+
---
+++ main.js
@@ -0,0 +1,41 @@
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12',
+ '#e74c3c', '#9b59b6', '#FB6964', '#342224', 
+ '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
+
+
+function chooseColor() {
+    var color = Math.floor(Math.random() * 13);
+    return colors[color];
+}
+
+
+
+function randomQoutes() {
+    $.ajax({
+        url: "https://api.forismatic.com/api/1.0/?",
+        dataType: "jsonp",
+        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+        success: function(data) {
+            $('.quote').html('"' + data.quoteText + '"');
+            $('.author').html(data.quoteAuthor);
+            $('.shareTwitter').on("click", function(){
+                var twtLink = 'https://twitter.com/home?status=' +encodeURIComponent(data.quoteText);
+                window.open(twtLink,'_blank');
+            })
+        },
+
+    })
+
+}
+
+$(document).ready(function(){
+    $('.newquote').on("click", function(){
+        randomQoutes();
+        var setColor = chooseColor();
+        $(this).css("background-color", setColor);
+        $('.shareTwitter').css("background-color", setColor);
+        $('body').css("background-color", setColor);
+        $('p').css("color", setColor);
+    });
+
+});
